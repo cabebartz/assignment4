d6e417fe11982d1aa199c50ab85f46241553742c
Cabe Bartz <cabebartz@fullsai.edu>
2012-06-17 14:28:19 -0700

    Added function that converts  number to 2 decimal places.  Changing the toFixed parameter will change how many decimal places.

--- bartz_cabe_assignment4.js
+++ bartz_cabe_assignment4.js
@@ -1,1 +1,12 @@
+//Cabe Bartz
+//SDI 1206
+//June 17th, 2012
+
 // alert("JavaScript works!");
+
+//var numMoney converts numbers to 2 places after decimal ie dollers
+var numMoney = function(dollar){
+	var dollar = dollar.toFixed(2);
+	return dollar;	
+};
+console.log(numMoney(2.1);
