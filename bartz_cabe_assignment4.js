5ada2de905fc1473d31bc5e1f8dfb2952fb4d868
Cabe Bartz <cabebartz@fullsai.edu>
2012-06-17 15:16:41 -0700

    added function phoneNumber that will check if a number is is a 000-000-0000 phone number format.  Will return true if it is.  False if it is not.

--- bartz_cabe_assignment4.js
+++ bartz_cabe_assignment4.js
@@ -4,8 +4,9 @@
 
 // alert("JavaScript works!");
 
-//numMoney converts numbers to 2 places after decimal ie dollers.  The
-//dollar argument is the number you want changed.  the toFixed parameter is how many places after decimal point.
+//numMoney converts numbers to 2 places after decimal ie dollers.  The dollar
+//argument is the number you want changed.  the toFixed parameter is how many
+//places after decimal point.
 var numMoney = function(dollar){
 	var dollar = dollar.toFixed(2);
 	return dollar;	
@@ -19,3 +20,16 @@ var stringNumber = function(string){
 	return string;
 };
 console.log(stringNumber("75.888"));
+
+//phoneNumber determines whether a string "num" is in a 000-000-0000 phone
+//number format.  will return true if it is or false if it is not
+var phoneNumber = function(num){
+	var num;
+	if ((num.length === 12) && (num.charAt(3) === "-") && (num.charAt(7) === "-") ){
+		return true;
+	} else {
+		return false;
+	};
+	return num;
+};
+console.log(phoneNumber("123-456-7890"))
