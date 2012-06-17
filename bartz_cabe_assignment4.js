507b8bcd62a60db72b7decf4cf6207063e9d0872
Cabe Bartz <cabebartz@fullsai.edu>
2012-06-17 14:43:32 -0700

    added function (stringNumber) that will convert a number inside of a string into a number

--- bartz_cabe_assignment4.js
+++ bartz_cabe_assignment4.js
@@ -4,9 +4,18 @@
 
 // alert("JavaScript works!");
 
-//var numMoney converts numbers to 2 places after decimal ie dollers
+//numMoney converts numbers to 2 places after decimal ie dollers.  The
+//dollar argument is the number you want changed.  the toFixed parameter is how many places after decimal point.
 var numMoney = function(dollar){
 	var dollar = dollar.toFixed(2);
 	return dollar;	
 };
-console.log(numMoney(2.1);
+console.log(numMoney(2.1));
+
+//stringNumber converts a number inside of a string into a number. string
+//argument is the string that will be converted to a number.
+var stringNumber = function(string){
+	var string = parseFloat(string);
+	return string;
+};
+console.log(stringNumber("75.888"));
