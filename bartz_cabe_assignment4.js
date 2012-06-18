b39d6870ebe8eb71f26beb38662a84199fa7be19
Cabe Bartz <cabebartz@fullsai.edu>
2012-06-17 18:36:26 -0700

    Fixed numerous spacing and syntax errors for first three functions.  Found on JSLint

--- bartz_cabe_assignment4.js
+++ bartz_cabe_assignment4.js
@@ -7,57 +7,56 @@
 //numMoney converts numbers to 2 places after decimal ie dollers.  The dollar
 //argument is the number you want changed.  the toFixed parameter is how many
 //places after decimal point.
-var numMoney = function(dollar){
-	var dollar = dollar.toFixed(2);
-	return dollar;	
+var numMoney = function (dollar) {
+	var convertDollar = dollar.toFixed(2);
+	return convertDollar;
 };
 console.log(numMoney(2.1));
 
 //stringNumber converts a number inside of a string into a number. string
 //argument is the string that will be converted to a number.
-var stringNumber = function(string){
-	var string = parseFloat(string);
-	return string;
+var stringNumber = function (string) {
+	var convertString = parseFloat(string);
+	return convertString;
 };
 console.log(stringNumber("75.888"));
 
 //phoneNumber determines whether a string "num" is in a 000-000-0000 phone
 //number format.  will return true if it is or false if it is not
-var phoneNumber = function(num){
-	var num;
-	if ((num.length === 12) && (num.charAt(3) === "-") && (num.charAt(7) === "-") ){
-		return true;
+var phoneNumber = function (num) {
+	var checkNum;
+	if ((num.length === 12) && (num.charAt(3) === "-") && (num.charAt(7) === "-")) {
+		checkNum = true;
 	} else {
-		return false;
-	};
-	return num;
+		checkNum = false;
+	}
+	return checkNum;
 };
 console.log(phoneNumber("123-456-7890"));
 
+
 //emailAddress determines whether a string follows the aaa@bbb.ccc format.  If
 //so, it will return true.  If not, it will return false.
-var emailAddress = function(string){
-	var string;
-	if ((string.indexOf(".") > string.indexOf("@")) && (string.match("@") != null) ){
+var emailAddress = function (string) {
+	if ((string.indexOf(".") > string.indexOf("@")) && (string.match("@") !== null)) {
 		return true;
 	} else {
 		return false;
-	};
+	}
 	return string;
 };
 console.log(emailAddress("cabebartz@fullsail.edu"));
 
 //isUrl determines whether a string is a URL that starts with either http: or
 //https:.  It returns true if it is or false if it is not.
-var isUrl = function(string){     
-	var string;     
+var isUrl = function (string) {        
 	if ((string.charAt(0) ==="h") && (string.charAt(1) === "t") && (string.charAt(2) === "t") &&
 	(string.charAt(3) === "p") && (string.charAt(4) === ":")){
 		return true;
 	} else if ((string.charAt(0) ==="h") && (string.charAt(1) === "t") && (string.charAt(2) === "t") &&
-	(string.charAt(3) === "p") && (string.charAt(4) === "s") && ((string.charAt(5) === ":"))){
+	(string.charAt(3) === "p") && (string.charAt(4) === "s") && ((string.charAt(5) === ":"))) {
 		return true;
-	} else{
+	} else {
 		return false;
 	};
 	return string;
@@ -67,11 +66,11 @@ console.log(isUrl("https:google.com"));
 //sumArray finds a total value of nummbers in an array.  If and array
 //parameter is NaN, sumArray will just add the numbers.  If strings in array
 //are numbers, it will not add them.  Would need to parseFloat array if that is desired.
-var sumArray = function(array){
+var sumArray = function (array) {
 	var arrayTotal = 0;
-	for(var i = 0; i < array.length; i++){
+	for(var i = 0; i < array.length; i++) {
 		var numArray = array[i];
-		if (!isNaN(numArray)){
+		if (!isNaN(numArray)) {
 			arrayTotal += numArray;
 		};	
 	};
